3c9eac5cab632ff23cac86d40d4dd10055dfc05d
unknown <giselanyelentorres@gmail.com>
2023-01-29 20:37:39 -0300

    primer comit

---
+++ Ejemplo1.java
@@ -0,0 +1,14 @@
+
+public class Ejemplo1{
+
+  public static void main(String[] args) {
+
+
+	  System.out.println("Hola mundo");
+	   
+	  double variable1 = 230.89;
+      int variableEntero = (int) variable1; //Cast
+  
+      System.out.println(variable1Entero);
+  }
+}
